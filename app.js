9f975b264fa892aa44173e5e20f8fc3046ebb339
Jeronimo Renaldi <jeroslp@gmail.com>
2022-12-20 22:37:30 -0300

    subiendo proyecto Mercado Liebre modificado

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ app.use('/static', express.static(__dirname + '/public'))
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log("corriendo en el puerto 3000")
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
 })
+
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/home.html')
